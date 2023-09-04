c5b576ce3ba58064e352b0e730302c5ed3b418f8
Khatchi <solomon9ice2014@gmail.com>
2023-09-04 14:30:21 -0700

    ...BaseModel class updatedby re-creating an instance with new dict repr task4

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,24 @@ class BaseModel:
     other classes, from which they inherit.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This func initializes the instance attributes of the
         BaseModel class.
         """
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """This func returns the string representations
