6d4d767fc0639a5f85b1c30b37e0a91432efee22
Khatchi <solomon9ice2014@gmail.com>
2023-09-04 11:43:18 -0700

    ...a dup for baseclass created for debugging

---
+++ models/dup_origin.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+This is the script for BaseModel Class.
+"""
+
+import uuid
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """ This class contains functionalities common to all
+    other classes, from which they inherit.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """This func initializes the instance attributes of the
+        BaseModel class.
+
+        ->self: Instance of the class being created
+        ->*args: list of positional arguments
+        ->**kwargs: collects keyword-args as dictionary
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            slef.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """This func returns the string representations
+        of the objs.
+        """
+
+        return ("[{}], ({}), {}".format
+                (type(self).__name__, self.id, self.__dict__))
+
+    def save(self):
+        """This func updataes the pub instance attri 'updated_at'
+        with the current datetime.
+        """
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """ This func returns as dict containing all keys/values-
+        pairs of the __dict__ instance.
+        """
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+
+        return (my_dict)
